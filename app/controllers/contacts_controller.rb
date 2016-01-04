d53ad8774b08b74cadee421d0efd9e5c0cbd40ae
Andrey Solodov <legandr.86@gmail.com>
2016-01-05 02:37:57 +0400

    contacts page with raise exception validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,16 @@
 class ContactsController < ApplicationController
   def process_form
+    if params[:contact][:name].blank?
+      raise 'Name is blank'
+    end
+    if params[:contact][:email].blank?
+      raise 'Email is blank'
+    end
+    if params[:contact][:content].blank?
+      raise 'Message is blank'
+    end
     Rails.logger.debug "DEBUG: params are #{params}"
-    flash[:notice] = "Received request from #{params[:contact][:name]}"
-    redirect_to root_path
+    message = "Received request from #{params[:contact][:name]}"
+    redirect_to root_path, :notice => message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  post 'contact', to: 'contacts#process_formgit'
+  post 'contact', to: 'contacts#process_form'
   root to: 'visitors#new'
 end
