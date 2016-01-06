29f1ec15a762cd7081128bde258c1f25db2df3da
Andrey Solodov <legandr.86@gmail.com>
2016-01-06 15:21:09 +0400

    contact form RailsWay

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,21 @@
 class ContactsController < ApplicationController
-  def process_form
-    if params[:contact][:name].blank?
-      raise 'Name is blank'
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(secure_params)
+    if @contact.valid?
+      flash[:notice] = "Message sent from #{@contact.name}."
+      redirect_to root_path
+    else
+      render :new
     end
-    if params[:contact][:email].blank?
-      raise 'Email is blank'
-    end
-    if params[:contact][:content].blank?
-      raise 'Message is blank'
-    end
-    Rails.logger.debug "DEBUG: params are #{params}"
-    message = "Received request from #{params[:contact][:name]}"
-    redirect_to root_path, :notice => message
+  end
+
+  private
+
+  def secure_params
+    params.require(:contact).permit(:name,:email,:content)
   end
 end
